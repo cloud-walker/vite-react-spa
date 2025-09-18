e15aeb238f1acb4753f3e692855e67dce54ea8c0
Luca Barone <baro.luc@gmail.com>
2025-09-18 23:56:30 +0200

    🏗️ upgrade setup (#1)
    * chore: add pnpm workspace configuration to specify only built dependencies
    
    * ♻️ Refactor project to remove Tailwind CSS and integrate Biome for linting and formatting
    
    * feat: update PostCSS configuration and integrate Panda CSS
    
    - Added '@pandacss/dev/postcss' plugin to PostCSS configuration.
    - Refactored App component to use styled-system for styling.
    - Introduced global CSS layers in global.css.
    - Updated tsconfig to include styled-system directory for TypeScript compilation.
    
    * Refactor code structure for improved readability and maintainability
    
    * pnpm dedupe
    
    * chore: add useNodeVersion to pnpm workspace configuration
    
    * chore: update packageManager field in package.json
    
    * chore: remove Prettier configuration and ignore file

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,11 +4,11 @@ import {App} from './App'
 import {render, screen, user} from './testing-library'
 
 test('works properly', async () => {
-  const handleClick = vi.fn()
-  render(<App onClick={handleClick} />)
+	const handleClick = vi.fn()
+	render(<App onClick={handleClick} />)
 
-  expect(screen.getByText('hello')).toBeInTheDocument()
+	expect(screen.getByText('hello')).toBeInTheDocument()
 
-  await user.click(screen.getByRole('button', {name: 'click me'}))
-  expect(handleClick).toHaveBeenCalledOnce()
+	await user.click(screen.getByRole('button', {name: 'click me'}))
+	expect(handleClick).toHaveBeenCalledOnce()
 })
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import {css} from '../styled-system/css'
+
 export function App({onClick}: {onClick?: () => void}) {
-  return (
-    <>
-      <h1 className="underline">hello</h1>
-      <button onClick={onClick}>click me</button>
-    </>
-  )
+	return (
+		<>
+			<h1 className={css({textDecoration: 'underline'})}>hello</h1>
+			<button type="button" onClick={onClick}>
+				click me
+			</button>
+		</>
+	)
 }
